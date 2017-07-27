21ad25a91560b0f97fde2404136a8246406ed862
bschwartz10 <bschwartz92@gmail.com>
2017-07-27 15:15:05 -0600

    limit to 10 links

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   validates :url, presence: true
 
   def self.hot_reads
-    where(updated_at: 24.hours.ago..Time.now).order(count: :desc)
+    where(updated_at: 24.hours.ago..Time.now).order(count: :desc).limit(10)
   end
 
 
